514598af0cd30e289c48732182a35e6349a1f2a6
Neel Kabra <kabraneel@gmail.com>
2022-11-23 02:13:35 +0530

    Added the splash Screen

---
+++ volunteering_app/lib/splash.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:volunteering_app/main.dart';
+
+class Splash extends StatefulWidget {
+
+  const Splash();
+
+  @override
+  _SplashState createState() => _SplashState();
+
+}
+
+class _SplashState extends State<Splash> {
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    goToHome();
+  }
+
+  void goToHome() async{
+    await Future.delayed(Duration(milliseconds: 1500), (){});
+    Navigator.of(context).pushReplacement(
+       MaterialPageRoute(builder: (BuildContext context) {
+        return MyHomePage();
+      })
+    );
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body : Center(
+        child : Container(
+          // ignore: prefer_const_constructors
+          child : (Text(
+              'Splash Screen',
+              // ignore: prefer_const_constructors
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'Roboto',
+              ),
+            )
+          ),
+        )
+      ,) 
+    );
+  }
+}
